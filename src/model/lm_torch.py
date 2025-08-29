a9ae70847f4d393483eb81c93a2026710d5a1ca6
ravindra-sonawane <ravindrapopat.sonawane@uniphore.com>
2025-08-30 00:48:45 +0530

    Add LM code using PyTorch Implementations

---
+++ src/model/lm_torch.py
@@ -0,0 +1,30 @@
+import torch
+
+class MarathiLM(torch.nn.Module):
+    def __init__(self, vocab_size: int, context_length: int, d_model: int, num_heads: int, n_layers: int, d_ff: int):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.context_length = context_length
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.n_layers = n_layers
+        self.d_ff = d_ff
+
+        self.embedding_layer = torch.nn.Embedding(self.vocab_size, self.d_model)
+        self.position_embedding = torch.nn.Embedding(self.context_length, self.d_model)
+        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self.d_model, nhead=self.num_heads,
+                                                         dim_feedforward=self.d_ff)
+        self.transformer_encoder = torch.nn.TransformerEncoder(encoder_layer, num_layers=self.n_layers,
+                                                               norm=torch.nn.RMSNorm(normalized_shape=self.d_model))
+        self.final_output_layer = torch.nn.Linear(in_features=self.d_model, out_features=self.vocab_size)
+
+    def forward(self, x):
+        token_embedding = self.embedding_layer(x)
+        position_embedding = self.position_embedding(x)
+        overall_embedding = token_embedding + position_embedding
+        output = self.transformer_encoder(overall_embedding)
+        output = self.final_output_layer(output)
+
+        return output
+
+
--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,5 +1,7 @@
 from tokenizers import Tokenizer, models, pre_tokenizers, decoders, trainers, processors
 from pathlib import Path
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 def train_tokenizer(file_path, output_path):
     tokenizer = Tokenizer(models.BPE())
@@ -26,17 +28,60 @@ def decode(token_ids, tokenizer_path):
     tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer.decode(token_ids)
 
+def split_data(file_path, train_data_path, valid_data_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    total_poems = data.split("<|endoftext|>")
+    train_poems, valid_poems = train_test_split(total_poems, test_size=0.2, random_state=0)
+    train_data = "<|endoftext|>".join(train_poems)
+    train_data = train_data.strip()
+    train_data = train_data + '\n' + "<|endoftext|>"
+    valid_data = "<|endoftext|>".join(valid_poems)
+    valid_data = valid_data.strip()
+    valid_data = valid_data + '\n' + "<|endoftext|>"
+
+    with open(train_data_path, 'w', encoding='utf-8') as f:
+        f.write(train_data)
+
+    with open(valid_data_path, 'w', encoding='utf-8') as f:
+        f.write(valid_data)
+
 
 if __name__ == '__main__':
     ROOT = Path(__file__).resolve().parent.parent.parent
     data_file = ROOT / "data" / "tukaram_gatha_overall_data.txt"
     output_path = ROOT / "artifacts" / "bpe_tokenizer_huggingface.json"
-    train_tokenizer(file_path=data_file, output_path=output_path)
+    # train_tokenizer(file_path=data_file, output_path=output_path)
+    #
+    # example_text = 'गव्हाराचें ज्ञान अवघा रजोगुण । सुखवासी होऊन विषय भोगी ॥1॥'
+    # print('Length of input example is: {}'.format(len(example_text)))
+    # encoded_text = encode(example_text, output_path)
+    #
+    # decoded_text = decode(encoded_text.ids, output_path)
+    #
+    # assert decoded_text == example_text
 
-    example_text = 'गव्हाराचें ज्ञान अवघा रजोगुण । सुखवासी होऊन विषय भोगी ॥1॥'
-    print('Length of input example is: {}'.format(len(example_text)))
-    encoded_text = encode(example_text, output_path)
+    train_data_path = ROOT / "data" / "tukaram_gatha_train_data.txt"
+    valid_data_path = ROOT / "data" / "tukaram_gatha_valid_data.txt"
 
-    decoded_text = decode(encoded_text.ids, output_path)
+    split_data(data_file, train_data_path, valid_data_path)
 
-    assert decoded_text == example_text
+    with open(train_data_path, 'r', encoding='utf-8') as f:
+        train_data = f.read()
+
+    train_data_encoded = encode(train_data, output_path)
+    train_data_encoded = np.array(train_data_encoded.ids, dtype=np.int32)
+    np.save(ROOT / "data" / "tukaram_gatha_train_encoded.npy", train_data_encoded)
+
+    with open(valid_data_path, 'r', encoding='utf-8') as f:
+        valid_data = f.read()
+
+    valid_data_encoded = encode(valid_data, output_path)
+    valid_data_encoded = np.array(valid_data_encoded.ids, dtype=np.int32)
+    np.save(ROOT / "data" / "tukaram_gatha_valid_encoded.npy", valid_data_encoded)
+
+
+
+
+
